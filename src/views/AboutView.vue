73e624417ff63b7aa32225e0c9291f07404980f6
Alex Durso <alex.durso@listrak.com>
2022-07-26 11:31:56 -0400

    Initial commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify ToDo</h1>
+    <p>Vuetify ToDo is a simple application made by Alex Durso for tracking your ToDo list.
+       It is made using VueJs + Vuetify.
+    </p>
   </div>
 </template>
